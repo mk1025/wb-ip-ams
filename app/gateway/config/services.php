be8f863b300793b8fda647c775a25ad6c90c046b
Mark Roland Alueta <markintoshroland@gmail.com>
2026-02-28 21:38:06 +0800

    add configuration for auth and ip services

--- app/gateway/config/services.php
+++ app/gateway/config/services.php
@@ -35,4 +35,12 @@ return [
         ],
     ],
 
+    'auth' => [
+        'url' => env('AUTH_SERVICE_URL', 'http://localhost:8000'),
+    ],
+
+    'ip' => [
+        'url' => env('IP_SERVICE_URL', 'http://localhost:8001'),
+    ]
+
 ];
