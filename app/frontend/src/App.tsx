9788aff7025bfde408b8781ef529cefe323de3a1
Mark Roland Alueta <markintoshroland@gmail.com>
2026-03-02 22:02:22 +0800

    feat: refactor routing structure by moving routes to AppRoutes component

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,31 +1,12 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Toaster } from "./components/ui/sonner";
-import AuthLayout from "./components/layouts/auth-layout";
-import ProtectedRoute from "./components/protected-route";
-import DashboardLayout from "./components/layouts/dashboard-layout";
-import NotFoundPage from "./pages/not-found-page";
-import LoginPage from "./pages/login/page";
+import AppRoutes from "./routes";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AuthLayout />}>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<div>Register Page</div>} />
-        </Route>
-
-        <Route element={<ProtectedRoute />}>
-          <Route element={<DashboardLayout />}>
-            <Route path="/dashboard" element={<div>Dashboard Content</div>} />
-          </Route>
-        </Route>
-
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <AppRoutes />
       <Toaster
         richColors
         position="bottom-right"
---
+++ app/frontend/src/routes/index.tsx
@@ -0,0 +1,29 @@
+import AuthLayout from "@/components/layouts/auth-layout";
+import DashboardLayout from "@/components/layouts/dashboard-layout";
+import ProtectedRoute from "@/components/protected-route";
+import LoginPage from "@/pages/login/page";
+import NotFoundPage from "@/pages/not-found-page";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      {/* Public */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<div>Register page</div>} />
+      </Route>
+
+      {/* Protected */}
+      <Route element={<ProtectedRoute />}>
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<div>Dashboard page</div>} />
+        </Route>
+      </Route>
+
+      {/* Redirects */}
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
