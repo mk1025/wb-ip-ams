5d92bc0c54e21557ad4e4c540ab6ab872c12b6d6
Mark Roland Alueta <markintoshroland@gmail.com>
2026-03-02 22:11:39 +0800

    feat: update Card component width in LoginPage

--- app/frontend/src/pages/login/page.tsx
+++ app/frontend/src/pages/login/page.tsx
@@ -46,7 +46,7 @@ export default function LoginPage() {
   }
 
   return (
-    <Card className="w-full max-w-md">
+    <Card className="w-full max-w-lg">
       <CardHeader>
         <CardTitle>Login</CardTitle>
         <CardDescription>
