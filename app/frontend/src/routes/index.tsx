6c1fdc3133bb7d559bb00c3103eac64e67c260bf
Mark Roland Alueta <markintoshroland@gmail.com>
2026-03-02 22:12:16 +0800

    feat: update Register route to use RegisterPage component

--- app/frontend/src/routes/index.tsx
+++ app/frontend/src/routes/index.tsx
@@ -3,6 +3,7 @@ import DashboardLayout from "@/components/layouts/dashboard-layout";
 import ProtectedRoute from "@/components/protected-route";
 import LoginPage from "@/pages/login/page";
 import NotFoundPage from "@/pages/not-found-page";
+import RegisterPage from "@/pages/register/page";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export default function AppRoutes() {
@@ -11,7 +12,7 @@ export default function AppRoutes() {
       {/* Public */}
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<div>Register page</div>} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
 
       {/* Protected */}
