4c05b6cb20741026aa3e90b568b6edb0e58133d6
Mark Roland Alueta <markintoshroland@gmail.com>
2026-03-08 03:52:52 +0800

    refactor: change role column type to enum for users table

--- app/ip-service/database/migrations/0001_01_01_000000_create_users_table.php
+++ app/ip-service/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('role')->default('user');
+            $table->enum('role', ['user', 'super-admin'])->default('user');
             $table->string('password');
             $table->timestamps();
         });
