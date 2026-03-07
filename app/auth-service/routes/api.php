6e3dda65ca2346e28c81b9f173c5d989715fa401
Mark Roland Alueta <markintoshroland@gmail.com>
2026-03-08 02:23:11 +0800

    remove unused codeblock in auth service api

--- app/auth-service/routes/api.php
+++ app/auth-service/routes/api.php
@@ -2,12 +2,7 @@
 
 use App\Http\Controllers\Api\AuthAuditLogController;
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::prefix('auth')->group(function () {
     // Public
